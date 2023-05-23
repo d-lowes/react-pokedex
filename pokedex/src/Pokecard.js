b3c4f8444d3d861ff653cb6ad698165408c7491d
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-05-22 17:25:09 -0700

    pokedex app and pokecard components completed

--- pokedex/src/Pokecard.js
+++ pokedex/src/Pokecard.js
@@ -1,5 +1,14 @@
-
 const BASE_IMG_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+
+/**
+ * Props:
+ *      id,
+ *      name,
+ *      type,
+ *      base_experience.
+ *
+ * Returns a div of a single Pokemon, with their name, image, and type.
+ */
 
 function Pokecard({id, name, type, base_experience}) {
   return (
--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -1,5 +1,6 @@
 import Pokecard from "./Pokecard";
-/* [
+
+const DEFAULT_POKEMON_LIST = [
   {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
   {id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
   {id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
@@ -8,13 +9,33 @@ import Pokecard from "./Pokecard";
   {id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
   {id: 94,  name: 'Gengar',     type: 'poison',   base_experience: 225},
   {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
-]
-*/
+];
 
-function Pokedex(props) {
+/** Takes an array of objects describing different pokemon
+ *
+ *  props:
+ *    pokemon: an array of pokemon
+ *
+ * Renders a sequence of Pokecard components
+ *
+ * App > Pokedex
+ * Pokedex > Pokecard
+ *
+ */
+
+function Pokedex({ pokemon=DEFAULT_POKEMON_LIST }) {
   return (
-
-  )
+    <div className="Pokedex">
+    {pokemon.map(p => (
+      <Pokecard
+        id={p.id}
+        name={p.name}
+        type={p.type}
+        base_experience={p.base_experience}
+      />
+    ))}
+  </div>
+  );
 }
 
 export default Pokedex;
